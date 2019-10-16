dc202119476509cfbf593f78b64a478143b3af42
Nate Dietz <nsdietz@gmail.com>
2019-10-15 21:23:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching (drivers, name) {
+	return drivers.filter( function (driver) { return driver.toLowerCase().indexOf(name.toLowerCase()) !== -1; })
+}
+
+function fuzzyMatch (drivers, char) {
+	return drivers.filter( function (driver) { return driver.startsWith(char); })
+}
+
+function matchName(drivers, name) {
+	return drivers.filter( function (driver) { return driver.name === name; })
+}
