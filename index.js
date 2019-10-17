0475c24aab4c59e1b58b9a5d776a800244145cc8
epic0448 <samuelogundiran@Yahoo.com>
2019-10-16 20:40:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(x, name) {
+    //drivers is an arrat giving to use.
+    const matchFound = x.filter(xt => {
+         return xt.toLowerCase() === name.toLowerCase();
+        // console.log (xt === name)
+    })
+    return matchFound
+}
+
+
+function fuzzyMatch(x, letter) {
+    let checkDriver = x.filter(xt =>{return xt.toUpperCase().slice(0,letter.length) === letter.toUpperCase()})
+    return checkDriver
+}
+    
+       
+
+function matchName(array, name) {
+    
+}
