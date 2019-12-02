6c810db24b5fdaad82104cd801a662b67fc1e89d
Rebecca Tong <rtong@buffalo.edu>
2019-12-02 18:46:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers,string){
+let result = drivers.filter(word => word.toUpperCase() === string.toUpperCase())
+return result
+}
+
+function fuzzyMatch(drivers,string){
+  let result = drivers.filter(word => word == string)
+return result
+
+}
+
+
+function matchName(drivers,string){
+  let result = drivers.filter(word => word.name === string)
+  return result
+
+}
