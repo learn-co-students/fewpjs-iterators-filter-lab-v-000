8ae52f2bec55f3b8e079d2901223b2b74832ef91
Thomas Gray <thomas.gray@tgray.fios-router.home>
2019-12-13 07:40:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, search) {
+  return drivers.filter(name => name.toLowerCase() === search.toLowerCase())
+}
+
+function fuzzyMatch(drivers, search) {
+  return drivers.filter(name => name.substring(0, search.length) === search)
+}
+
+function matchName(drivers, search) {
+  return drivers.filter(driver => driver.name === search)
+}
