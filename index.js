78f78dd845c649eeed7aa64e298b36d65aaf49bc
Leslie Stevens <flymaven@protonmail.com>
2020-01-03 13:44:38 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(sourceArray, query) {
+  return sourceArray.filter(name =>
+  name.toLowerCase() === query.toLowerCase())
+}
+
+function fuzzyMatch(sourceArray, query) {
+  return sourceArray.filter(name => name.startsWith(query))
+}
+
+function matchName(sourceArray, query) {
+  return sourceArray.filter(driver => driver.name === query)
+}
