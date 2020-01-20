283a9c20df6d0bfd7c53f4555d553fecb9d8b782
MDAM182 <mdam2345@gmail.com>
2020-01-19 20:13:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+function findMatching(drivers, name){
+  return drivers.filter(n =>{
+    return n.toUpperCase() === name.toUpperCase()
+  });
+}
+
+function fuzzyMatch(drivers, el){
+    return drivers.filter(n => {
+        if (n.startsWith(el)){
+          return n
+      };
+    });
+}
+
+
+function matchName(drivers, name){
+    return drivers.filter(n => {
+        if  (n.name === name){
+            return n
+    };
+  });
+}
