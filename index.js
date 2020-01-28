491b588e4f754b858196dc4c4d1b4f678c5e0a79
Sushi Chanrai <sushi@pathworks.co>
2020-01-28 17:16:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(arr, string){
+  let result = arr.filter(word => {
+    return word.localeCompare(string, undefined, { sensitivity: 'accent' }) === 0
+    //or use regexp with match -> word.match(/ain/g)
+  })
+  return result
+}
+
+
+function fuzzyMatch(arr,letters){
+  let result = arr.filter(item => {
+  return item.startsWith(letters )? item : null
+  })
+  return result
+}
+
+
+function matchName(arr, name){
+  const result = arr.filter(item => {
+    return item.name === 'Bobby'
+  })
+  return result
+}
