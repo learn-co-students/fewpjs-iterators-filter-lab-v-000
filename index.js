a5b1f4780d5b030eaab0e783e8e9b5b1727b5445
daniellecroxton <danikrist@gmail.com>
2020-01-31 10:48:20 -0700

    Done.

--- index.js
+++ index.js
@@ -15,9 +15,3 @@ function matchName(drivers, string) {
         return driver.name.toLowerCase().indexOf(string.toLowerCase()) !== -1
     })
 }
-
-// function filterItems(arr, query) {
-//     return arr.filter(function(el) {
-//         return el.toLowerCase().indexOf(query.toLowerCase()) !== -1
-//     })
-//   }
