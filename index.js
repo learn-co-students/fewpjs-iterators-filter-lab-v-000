09178ee45826013874ec8f593c425f82c194b422
GiancarloZ <gzabaleta48@gmail.com>
2020-02-03 13:28:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(arr, str){
+    return arr.filter(n => {
+        return n.toLowerCase() === str.toLowerCase();
+    });
+}
+
+function fuzzyMatch(arr, str){
+    let strLength = str.length;
+    return arr.filter(n => {
+        return n.slice(0, strLength) === str;
+    });
+}
+
+function matchName(arr, str){
+    return arr.filter(n => {
+       return n.name.toLowerCase() === str.toLowerCase();
+    });
+}
