fd7f6267817d1eba51141698125f229bc64a4a38
Bruce Hillsworth <brucehillsworth@Air.local>
2020-02-08 17:00:53 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(array, query) {
+    return array.filter(element => element.toUpperCase() === query.toUpperCase());
+};
+
+function fuzzyMatch(array, query) {
+  return array.filter(element => element.slice(0, query.length) === query);
+};
+
+function matchName(array, query) {
+    return array.filter(element => element.name.toUpperCase() === query.toUpperCase());
+};
