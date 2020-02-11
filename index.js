eb2271ccbcdd926a2e818e7789cc8184343db86e
shktkt219 <shktkt219@gmail.com>
2020-02-11 22:25:15 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+function findMatching(drivers, name){
+    let matching = drivers.filter(d =>{
+        return d.toLowerCase() === name.toLowerCase();
+    });
+    return matching;
+}
+
+function fuzzyMatch(drivers, name){
+    let matching = drivers.filter(d =>{
+        return d.charAt(0).toLowerCase() === name.charAt(0).toLowerCase();
+    });
+    return matching; 
+}
+
+function matchName(drivers, nameSample){
+    let matching = drivers.filter(d =>{
+        return d.name.toLowerCase() === nameSample.toLowerCase();
+    });
+    return matching;
+}
