2d54e56ef4f3bb483f39d0970649ea57da7d6a24
danetter17 <etter.dt@gmail.com>
2020-02-19 17:18:10 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(listOfNames, query) {
+    return listOfNames.filter(element => element.toLowerCase() === query.toLowerCase());
+};
+
+function fuzzyMatch(listOfNames, query) {
+  return listOfNames.filter(element => element.slice(0, query.length) === query);
+};
+
+function matchName(listOfNames, query) {
+    return listOfNames.filter(element => element.name.toLowerCase() === query.toLowerCase());
+};
