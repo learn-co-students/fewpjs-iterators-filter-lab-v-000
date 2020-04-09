de15006d4abab0d1dfcf1a770ab11b0b94b8ca2c
Kat Tochowicz <ktochow1@binghamton.edu>
2020-04-09 18:59:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(driversArr, name){
+    return driversArr.filter(n => n.toLowerCase() === name.toLowerCase())
+  };
+
+function fuzzyMatch(driversArr, name){
+  return driversArr.filter(n => n.charAt(0) === name.charAt(0))
+}
+
+function matchName(driversArr, name){
+  return driversArr.filter(n => n.name === name)
+}
