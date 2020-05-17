c30737a0c023797510008c681e8567b19e2f2792
sdcro <sdcrouse91@gmail.com>
2020-05-17 15:49:31 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, driverName) {
+  return drivers.filter( driver => driver.toLowerCase() === driverName.toLowerCase() );
+}
+
+function fuzzyMatch(drivers, startingLetters) {
+  return drivers.filter( driver => driver.startsWith(startingLetters) );
+}
+
+function matchName(drivers, driverName) {
+  return drivers.filter( driver => driver.name === driverName );
+}
