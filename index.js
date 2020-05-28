15ec479194455d24d49a481de9570c6ff1ee1b00
sjwakeman <swakeman@ca.rr.com>
2020-05-28 09:45:04 -0700

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,13 @@
 //     })
 //   }
 
+// Write findMatching - This function takes an array of drivers and a string as arguments, 
   function findMatching(drivers, driver) {
     // console.log(drivers, "drivers") 
     // [ 'Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby' ] drivers
     // console.log(driver, "driver")
     // Bobby driver
+    // returns the matching list of drivers.
     return drivers.filter(function(name) {
         // console.log(name)
         // Bobby
@@ -23,6 +25,15 @@
         // Annette
         // Sarah
         // bobby
+        return name.toLowerCase().indexOf(driver.toLowerCase()) !== -1
+    })
+  }
+
+// Write fuzzyMatch - This function takes an array of drivers and a string 
+// as arguments for querying the array, 
+// and returns all drivers whose names begin with the provided letters.
+  function fuzzyMatch(drivers, driver) {
+    return drivers.filter(function(name) {
         return name.toLowerCase().indexOf(driver.toLowerCase()) !== -1
     })
   }
