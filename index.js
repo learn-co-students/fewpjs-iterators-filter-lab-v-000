f87ba2c99562aea1d525b34ae29c659c280fcefc
benitolsantos <github email address>
2020-06-08 12:03:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+//nmp test is faster <- REMEMBER THIS
+
+function findMatching(drivers, name) {
+  //console.log(drivers)
+  //console.log(name)
+  //console.log(drivers.filter(n => {return n === (name || name.toDownCase)}))
+  let matchingName = drivers.filter(n => {
+    return n === name || n === name.toLowerCase() || n === name.toUpperCase();
+  }); //should return array of matching
+  //only return works once, remember to make the methods with () <-- remember this! Kevin Webster
+  return matchingName;
+}
+
+function fuzzyMatch(drivers,name) {
+ console.log(drivers)
+ console.log(name)
+ let matchingName = drivers.filter(n => {
+     return n[...1] === name[...1] //using the spread operator      
+ })
+}
