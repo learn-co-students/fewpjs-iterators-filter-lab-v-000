8015e8d33cda8fe68db1f9c050ff4948168a5d01
ADogTheWarrior <coolfudge@gmail.com>
2020-09-03 12:39:35 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(array, name){
+  return array.filter(s => (s.charAt(0).toUpperCase() + s.slice(1) || s.charAt(0).toLowerCase() + s.slice(1)) == name)
+}
+function fuzzyMatch(array, string){
+  return array.filter(s => s.indexOf(string) == 0)
+}
+function matchName(array, name) {
+  return array.filter(s => s.name === name)
+}
