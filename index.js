4fb884dbb24e7ba4d62bd0a663725ea30e1503d9
Eric Johnson <ericjohnson0216@icloud.com>
2020-10-04 11:37:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(list, name) {
+    return list.filter((driverName) => {
+        return driverName.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch(list, name) {
+    
+    let x =  name.length 
+    return list.filter((driverLetters) => {
+        return driverLetters.slice(0, x) === name;
+    });
+    
+}
+
+function matchName(name) {
+}
