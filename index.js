2f8ab1b4b163ccb154e73f3deaf2cc7e25bd9d34
tscotce <tatyana.celovsky@gmail.com>
2020-12-03 18:16:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(arr, name) {
+    return arr.filter(e => {
+        return e === name;
+    });
+}
+
+function fuzzyMatch(arr, name) {
+    return arr.filter(e => {
+        return e[1] === name;
+    });
+}
+
+// function matchName(arr, name) {
+//     return arr.filter(function (e) {
+//         return e.name === name;
+//     });
+// }
+
+function matchName(arr, name) {
+    return arr.filter(e => e.name === name);
+}
