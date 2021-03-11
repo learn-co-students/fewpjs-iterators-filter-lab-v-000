5c1b738a07ba62f9a1d8dfa75df259e9dae8a14f
github username <d24dust@hotmail.com>
2021-03-11 13:55:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, query) {
+  return drivers.filter((driver) => RegExp(query, "i").test(driver) )
+}
+
+function fuzzyMatch(drivers, query) {
+  return drivers.filter((driver) => RegExp(`^${query}`, "i").test(driver) )
+}
+
+function matchName(drivers, query) {
+  return drivers.filter((driver) => driver.name === query)
+}
