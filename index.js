5bb6e4cdbda39f2023f5d0220878aeb0ae64bad5
guysbryant <guy.s.bryant@gmail.com>
2021-05-03 20:34:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function upCaseAll(word){
+  return word.split(' ').map(chars => chars.split(' ').map(char => char.toUpperCase()).join(' ')).join(' ');
+}
+function findMatching(arr, s) {
+  // return arr.filter(e => upCaseAll(e) === upCaseAll(s));
+  return arr.filter(e => e.toUpperCase() === s.toUpperCase());
+}
+function fuzzyMatch(arr, s){
+  return arr.filter(e => e.charAt(0) === s.charAt(0));
+}
+const matchName = (arr, s) => arr.filter(e => e["name"] === s);
