5351a96545d2797fa3a0e158681ad62a3183f2c0
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-16 15:53:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+};
+
+function fuzzyMatch(drivers, substring) {
+    return drivers.filter(driver => driver.startsWith(substring));
+};
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
