9f7c36c093ddc0a65d6cec4bcae340da63dcd293
capricious-slingshot <ephemere@protonmail.com>
2021-05-16 19:20:23 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(arr, str){
+  return arr.filter( i => i.toLowerCase() === str.toLowerCase())
+}
+
+function fuzzyMatch(arr, str){
+  return arr.filter( i => i.substring(0,1).toLowerCase() === str.substring(0,1).toLowerCase())
+}
+
+function matchName(objects, str){
+  return objects.filter( obj => obj.name == str )
+}
